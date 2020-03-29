e9ec2d870d8b49df53a3bed70420c61d63ece360
craig fay <craigfay1@gmail.com>
2020-03-29 14:17:02 -0500

    Add h1n1 data/plotting

--- covid_19.py
+++ covid_19.py
@@ -1,4 +1,5 @@
 # Libraries
+import math
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -10,33 +11,66 @@ pd.options.display.max_rows = 100
 # Suppress scientific notation
 np.set_printoptions(suppress=True)
 
-# Importing Dataset
-covid_19 = pd.read_csv('./covid_19.csv', 
-                       parse_dates=['Date'])
-
-# Selecting desired columns
-covid_19 = covid_19[['Date', 'Country/Region', 'Confirmed', 'Deaths', 'Recovered']]
-
-# Create a synthetic paramter, Nth_day
-covid_19['Nth_day'] = (covid_19['Date'] - min(covid_19['Date'])).dt.days
-
-
-# Collapse all records that have the same nth_day onto one record
-total_days = covid_19['Nth_day'].at[len(covid_19['Nth_day']) -1]
-data = np.zeros(shape=(total_days + 1, 3))
-for n, row in covid_19.iterrows():
-    nth_day = row['Nth_day']
-
-    data[nth_day][0] += row['Confirmed']
-    data[nth_day][1] += row['Deaths']
-    data[nth_day][2] += row['Recovered']
-c19 = pd.DataFrame(data=data, columns=['confirmed', 'deaths', 'recovered'])
-
-first_n_days = np.arange(0, total_days + 1) 
 
 # COVID-19
-plt.plot(first_n_days, c19['confirmed'], '--', color='red', label='Confirmed Cases')
-plt.plot(first_n_days, c19['deaths'], color='red', label='Deaths')
+# --------
+
+# Importing Dataset
+c19 = pd.read_csv('./covid_19.csv', parse_dates=['Date'])
+
+# Selecting desired columns
+c19 = c19[['Date', 'Country/Region', 'Confirmed', 'Deaths', 'Recovered']]
+
+# Create a synthetic paramter, Nth_day
+c19['Nth_day'] = (c19['Date'] - min(c19['Date'])).dt.days
+
+# Collapse all records that have the same nth_day onto one record
+c19_total_days = c19['Nth_day'].at[len(c19['Nth_day']) -1]
+# print(c19.tail(1)['Nth_day'])
+#c19_total_days = c19.tail(1)['Nth_day'][0]
+data = np.zeros(shape=(c19_total_days + 1, 2))
+for n, row in c19.iterrows():
+    nth_day = row['Nth_day']
+    data[nth_day][0] += row['Confirmed']
+    data[nth_day][1] += row['Deaths']
+c19 = pd.DataFrame(data=data, columns=['confirmed', 'deaths'])
+c19_first_n_days = np.arange(0, c19_total_days + 1) 
+
+
+# H1N1
+# ----
+
+# Importing Dataset
+h1n1 = pd.read_csv('./h1n1.csv', parse_dates=['Update Time'], encoding='ISO-8859-1')
+
+# Create a synthetic paramter, Nth_day
+h1n1['Nth_day'] = (h1n1['Update Time'] - min(h1n1['Update Time'])).dt.days
+h1n1 = h1n1.sort_values(by='Nth_day')
+
+# Collapse all records that have the same nth_day onto one record
+h1n1_total_days = h1n1.tail(1)['Nth_day'][0]
+data = np.zeros(shape=(h1n1_total_days + 1, 2))
+for n, row in h1n1.iterrows():
+    nth_day = row['Nth_day']
+    data[nth_day][0] += row['Cases']
+    data[nth_day][1] += row['Deaths']
+h1n1 = pd.DataFrame(data=data, columns=['confirmed', 'deaths'])
+h1n1_first_n_days = np.arange(0, h1n1_total_days + 1) 
+
+# Filter out missing values
+h1n1 = h1n1[h1n1['confirmed'] > 0]
+h1n1 = h1n1[h1n1['deaths'].notnull()]
+
+
+# Visualization
+# ------------
+
+plt.plot(c19['confirmed'], '--', color='red', label='C19 Cases')
+plt.plot(c19['deaths'], color='red', label='C19 Deaths')
+
+plt.plot(h1n1['confirmed'], '--', color='blue', label='H1N1 Cases')
+plt.plot(h1n1['deaths'], color='blue', label='H1N1 Deaths')
+
 plt.xlabel('First N Days')
 plt.ylabel('Population')
 plt.title('COVID-19')
